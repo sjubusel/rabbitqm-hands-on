c5dddaef039e96e96f953e94a891f7cb79fb422d
Siarhei Busel <siarhei.busel@innowise.com>
2025-06-26 13:46:25 +0200

    Make queue durable and messages persistent; add `basicAck` and `basicQos` handling in Consumer; update `docker-compose.yml` to include a second consumer instance.

--- modules/consumer/src/main/kotlin/Consumer.kt
+++ modules/consumer/src/main/kotlin/Consumer.kt
@@ -3,17 +3,27 @@ package com.github.sjubusel.app
 import com.github.sjubusel.utils.QUEUE_NAME
 import com.github.sjubusel.utils.initConnectionFactory
 import com.rabbitmq.client.DeliverCallback
+import java.time.LocalDateTime
 
 fun main() {
-    initConnectionFactory().newConnection().use { connection ->
+    initConnectionFactory().newConnection()?.let { connection ->
         val channel = connection.createChannel()
-        channel.queueDeclare(QUEUE_NAME, false, false, false, null)
-        println(" [*] Waiting for messages. To exit press CTRL+C")
+        val durable = true
+        channel.queueDeclare(QUEUE_NAME, durable, false, false, null)
+        val prefetchCount = 1
+        channel.basicQos(prefetchCount)
 
         val deliveryCallback = DeliverCallback { _, delivery ->
             val message = String(delivery.body)
-            println(" [x] Received '$message'")
+            println("[x] Received '$message' at ${LocalDateTime.now()}")
+            try {
+                Thread.sleep(5000L)
+            } finally {
+                channel.basicAck(delivery.envelope.deliveryTag, false)
+                println("[x] Processing is Done")
+            }
         }
-        channel.basicConsume(QUEUE_NAME, true, deliveryCallback, {})
+        val autoAck = false
+        channel.basicConsume(QUEUE_NAME, autoAck, deliveryCallback, {})
     }
 }
--- modules/publisher/src/main/kotlin/Publisher.kt
+++ modules/publisher/src/main/kotlin/Publisher.kt
@@ -2,14 +2,16 @@ package com.github.sjubusel.app
 
 import com.github.sjubusel.utils.QUEUE_NAME
 import com.github.sjubusel.utils.initConnectionFactory
+import com.rabbitmq.client.MessageProperties
 
 fun main() {
     initConnectionFactory().newConnection().use { connection -> // abstraction over sockets
         val channel = connection.createChannel()
-        channel.queueDeclare(QUEUE_NAME, false, false, false, null) // idempotent
+        val durable = true
+        channel.queueDeclare(QUEUE_NAME, durable, false, false, null) // idempotent
 
         val message = "Hello, World!"
-        channel.basicPublish("", QUEUE_NAME, null, message.toByteArray())
+        channel.basicPublish("", QUEUE_NAME, MessageProperties.PERSISTENT_TEXT_PLAIN, message.toByteArray())
         println("[+] Published a '$message'")
     }
 
