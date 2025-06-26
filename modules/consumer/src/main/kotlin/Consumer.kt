11cabbc566d710178542c5782d807df1a97ce289
Siarhei Busel <siarhei.busel@innowise.com>
2025-06-26 09:16:04 +0200

    Set up RabbitMQ integration with publisher and consumer modules

---
+++ modules/consumer/src/main/kotlin/Consumer.kt
@@ -0,0 +1,21 @@
+package com.github.sjubusel.app
+
+import com.github.sjubusel.utils.QUEUE_NAME
+import com.rabbitmq.client.ConnectionFactory
+import com.rabbitmq.client.DeliverCallback
+
+fun main() {
+    val factory = ConnectionFactory()
+    factory.host = "localhost"
+    factory.newConnection().use { connection ->
+        val channel = connection.createChannel()
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null)
+        println(" [*] Waiting for messages. To exit press CTRL+C")
+
+        val deliveryCallback = DeliverCallback { _, delivery ->
+            val message = String(delivery.body)
+            println(" [x] Received '$message'")
+        }
+        channel.basicConsume(QUEUE_NAME, true, deliveryCallback, {})
+    }
+}
---
+++ modules/publisher/src/main/kotlin/Publisher.kt
@@ -0,0 +1,17 @@
+package com.github.sjubusel.app
+
+import com.github.sjubusel.utils.QUEUE_NAME
+import com.rabbitmq.client.ConnectionFactory
+
+fun main() {
+    val connectionFactory = ConnectionFactory()
+    connectionFactory.host = "localhost"
+    connectionFactory.newConnection().use { connection -> // abstraction over sockets
+        val channel = connection.createChannel()
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null) // idempotent
+
+        val message = "Hello, World!"
+        channel.basicPublish("", QUEUE_NAME, null, message.toByteArray())
+    }
+
+}
